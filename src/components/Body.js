ea219c0118395d9f2c75f3275356b4cbf837f4f9
Pujitha <Pujitha.polu@cirium.com>
2024-11-06 20:42:04 +0000

    Contents do not change based on the data or config

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,8 +2,21 @@ import { restList } from "../utils/resList";
 import ResCard from "./ResCard";
 
 const Body = () => {
+    let listofRestaurants = restList;
     return (
         <div className='body'>
+            <div className='filter'>
+                <button className='filter-button' onClick={() => {
+                    /**
+                     * if we use restList=restList in place of listofRestaurants 
+                     * Cannot set property restList of #<Object> which has 
+                     * only a getter TypeError: Cannot set property restList 
+                     * of #<Object> which has only a getter at onClick
+                     */
+                    listofRestaurants = listofRestaurants.filter(restaurant => restaurant.data.avgRating > 4);
+                    console.log(restList);
+                }}>Top Rated Restaurants</button>
+            </div>
             <div className='search'>
                 Search
             </div>
